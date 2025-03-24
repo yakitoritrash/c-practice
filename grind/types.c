d056e811358800f4973f9f8c377f35b41efe47d1
Arch me <kishudwivedi30@gmail.com>
2025-03-24 19:14:00 +0530

    fdgdfg

---
+++ grind/types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+  
+  int max_recursive_calls = 100;
+  float throttle_speed = 0.2;
+  int ports = 65535;
+
+  printf("Max recursive call: %d\n", max_recursive_calls);
+  printf("No. of Ports: %d\n", ports);
+  printf("Throttle speed: %f\n", throttle_speed);
+
+  return 0;
+
+}
