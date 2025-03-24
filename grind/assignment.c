1f2b3aa319a99f467b59c1422728617cbb74a32b
Arch me <kishudwivedi30@gmail.com>
2025-03-24 23:21:58 +0530

    gsjhgfs

---
+++ grind/assignment.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(){
+  
+  printf("sizeof(bool) = %zu\n", sizeof(bool));
+  printf("sizeof(int) = %zu\n", sizeof(int));
+  printf("sizeof(float) = %zu\n", sizeof(float));
+  printf("sizeof(char) = %zu\n", sizeof(char));
+  printf("sizeof(double) = %zu\n", sizeof(double));
+
+}
