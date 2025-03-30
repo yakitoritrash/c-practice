1492594547f745ad0e939c10748666866eb3e193
Arch me <kishudwivedi30@gmail.com>
2025-03-30 15:26:01 +0530

    fgsdg

--- grind/pointers.c
+++ grind/pointers.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 // int *p, *q, *r;
+void increment(int *p) {
+  (*p)++;
+}
+
 int main() {
   int x = 42;
-  int *p = &x;
+  int *p = &x; //save x's addr to p
+  int **q = &p;
+  increment(p);
 
-  printf("%d ", *p);
+  printf("%d ", x); //print out value of x
+  return 0;
 }
