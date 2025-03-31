03342f912a8f19108d76266abf580242a1a04240
Arch me <kishudwivedi30@gmail.com>
2025-04-01 01:17:47 +0530

    dsfsdf

--- grind/concatstr.c
+++ grind/concatstr.c
@@ -9,7 +9,18 @@ void strconcat(char *str1, char *str2) {
 
     idx++;
   }
-  printf("Found null at %d for %s", idx, str1);
+
+  int str2_pointer = 0;
+  while (1) {
+    str1[idx + str2_pointer] = str2[str2_pointer];
+
+    if (str2[str2_pointer] == '\0') {
+      return;
+    }
+    str2_pointer++;
+  }
+
+  //printf("Found null at %d for %s", idx, str1);
 
 }
 
