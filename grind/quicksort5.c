6041c62feff28756012500360f4d5150c3948328
Arch me <kishudwivedi30@gmail.com>
2025-04-01 12:07:56 +0530

    dsfsdf

---
+++ grind/quicksort5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int partition(int arr[], int lo, int hi) {
+  int pivot = arr[hi];
+  int idx = lo - 1;
+
+  for (int i = lo; i <= hi; i++) {
+    if (arr[i] < pivot) {
+      idx++;
+      int tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+  return idx;
+}
+
+void qs(int arr[], int lo, int hi) {
+  if (lo >= hi) {
+    return
+  }
+
+  pivotIdx = partition(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+}
+
+void partition(int arr[], int size) {
+  qs(arr, 0, size - 1);
+}
+
+int main() {
+  int arr[] = {3, 4, 1, 5, 4, 3, 8, 9, 2};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  
+  partiton(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+}
