a35a80360b53b325a6b3052ac96563fa58e46901
Arch me <kishudwivedi30@gmail.com>
2025-04-02 10:32:42 +0530

    sdfsdf

---
+++ grind/bubblesort8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void bubblesort(int arr[], int size) {
+  for (int i = 0; i < size - 1; i++) {
+    for (int j = 0; j < size - 1 - i; j++) {
+      if (arr[j] > arr[j+1]) {
+        int tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+}
+
+int main() {
+  int arr[] = {4, 3, 1, 7, 5, 6, 7, 6, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  bubblesort(arr, size);
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+}
