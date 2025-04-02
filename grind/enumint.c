b1d493b749d1af075a1ae19351f1ecdb1829875f
Arch me <kishudwivedi30@gmail.com>
2025-04-02 19:19:08 +0530

    sdffsdf

--- grind/enumint.c
+++ grind/enumint.c
@@ -1,4 +1,4 @@
-#include <stdio.h>Bignumbers 
+#include <stdio.h> 
 
 typedef enum {
   BIG = 1234123412341234,
@@ -7,11 +7,11 @@ typedef enum {
 } BigNumbers;
 
 typedef enum {
-  HTTP BAD_REQUEST = 400,
+  HTTP_BAD_REQUEST = 400,
   HTTP_UNAUTHORIZED = 401,
   HTTP_NOT_FOUND = 404,
   HTTP_I_AM_A_TEAPOT = 418,
-  HTTP_INTERNAL_SERVER_ERROR = 500
+  HTTP_INTERNAL_SERVER_ERROR = 500,
 } HttpErrorCode;
 
 int main() {
