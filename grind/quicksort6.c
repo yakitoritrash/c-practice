a7e811dd5648294098d6183922976c4ca71fb1f2
Arch me <kishudwivedi30@gmail.com>
2025-04-02 10:58:45 +0530

    dsfsfd

---
+++ grind/quicksort6.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int partition(int arr[], int lo, int hi) {
+
+  int pivot = arr[hi];
+  int idx = lo - 1;
+
+  for (int i = lo; i < hi; i++) {
+    if (arr[i] < pivot) {
+      idx++;
+      int tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+  return idx;
+}
+
+void qs(int arr[], int lo, int hi) {
+  if (lo >= hi) {
+    return;
+  }
+
+  int pivotIdx = partition(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+}
+
+void quicksort(int arr[], int size) {
+  qs(arr, 0, size - 1);
+}
+
+int main() {
+  int arr[] = {3, 4, 2, 1, 5, 4, 6, 7, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  quicksort(arr, size);
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+}
+
