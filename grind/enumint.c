047e0c98f61343923eb54f18638741bf444e78f9
Arch me <kishudwivedi30@gmail.com>
2025-04-02 19:16:39 +0530

    sdfdsf

---
+++ grind/enumint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>Bignumbers 
+
+typedef enum {
+  BIG = 1234123412341234,
+  BIGGER,
+  BIGGEST,
+} BigNumbers;
+
+typedef enum {
+  HTTP BAD_REQUEST = 400,
+  HTTP_UNAUTHORIZED = 401,
+  HTTP_NOT_FOUND = 404,
+  HTTP_I_AM_A_TEAPOT = 418,
+  HTTP_INTERNAL_SERVER_ERROR = 500
+} HttpErrorCode;
+
+int main() {
+  
+  printf("The size of Bignumbers is %zu bytes\n", sizeof(BigNumbers));
+  printf("The size of HttpErrorCode is %zu bytes\n", sizeof(HttpErrorCode));
+    
+  return 0;
+
+}
