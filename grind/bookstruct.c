995d1f85849b3372dfefe7ca497c016d16af7cec
Arch me <kishudwivedi30@gmail.com>
2025-04-04 10:28:58 +0530

    sfdsdf

---
+++ grind/bookstruct.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+  char *title;
+  char *author;
+  float price;
+} Book;
+
+int main() {
+  Book a {
+    a.title = lotr;
+    a.author = tolkein;
+    a.price = 25.25;
+  }
+}
