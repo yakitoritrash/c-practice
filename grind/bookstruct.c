ab5bff4b93bc9428d1eaa9c1140162eb525e0a6a
Arch me <kishudwivedi30@gmail.com>
2025-04-04 10:40:51 +0530

    sfdsdf

--- grind/bookstruct.c
+++ grind/bookstruct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct {
   char *title;
@@ -7,9 +8,24 @@ typedef struct {
 } Book;
 
 int main() {
-  Book a {
-    a.title = lotr;
-    a.author = tolkein;
-    a.price = 25.25;
-  }
+  Book a; 
+  a.title = "lotr";
+  a.author = "tolkein";
+  a.price = 25.25;
+  
+  Book *b = malloc(sizeof(Book));
+
+  b->title = "doawk";
+  b->author = "kinney";
+  b->price = 32.32;
+
+  printf("Title of book a: %s\n", a.title);
+  printf("Author f book a: %s\n", a.author);
+  printf("Title of book a: $%.2f\n", a.price);
+
+  printf("Title of book b: %s\n", b->title);
+  printf("Author f book b: %s\n", b->author);
+  printf("Title of book b: $%.2f\n", b->price);
+  return 0;
+
 }
