f2f0282d98867a70d50b27653667f92582253f7d
Arch me <kishudwivedi30@gmail.com>
2025-04-04 10:49:26 +0530

    Sdfs

--- grind/bookstruct.c
+++ grind/bookstruct.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 typedef struct {
@@ -14,18 +15,27 @@ int main() {
   a.price = 25.25;
   
   Book *b = malloc(sizeof(Book));
+  if (b == NULL) {
+    printf("Memory allocation failed.\n");
+    return 1;
+  }
 
-  b->title = "doawk";
+  b->title = malloc(20);
+  strcpy(b->title, "doawk");
   b->author = "kinney";
   b->price = 32.32;
+  
 
   printf("Title of book a: %s\n", a.title);
   printf("Author f book a: %s\n", a.author);
-  printf("Title of book a: $%.2f\n", a.price);
+  printf("Price of book a: $%.2f\n", a.price);
 
   printf("Title of book b: %s\n", b->title);
-  printf("Author f book b: %s\n", b->author);
-  printf("Title of book b: $%.2f\n", b->price);
-  return 0;
+  printf("Author of book b: %s\n", b->author);
+  printf("Price of book b: $%.2f\n", b->price);
 
+  free(b->title);
+  free(b);
+
+  return 0;
 }
