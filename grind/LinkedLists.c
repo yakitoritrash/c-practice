41b5842c21073b572263b85c60a82cdf4fbf4e30
Arch me <kishudwivedi30@gmail.com>
2025-04-05 10:27:57 +0530

    DfsFD

--- grind/LinkedLists.c
+++ grind/LinkedLists.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node_t{
+  int data;
+  struct node_t *next;
+} node_t;
+
+node_t *create_node(int value) {
+  node_t *new_node = malloc(sizeof(node_t));
+
+  if (!new_node) {
+    printf("Memory Allocation failed.\n");
+    return NULL;
+  }
+
+  new_node->data = value;
+  new_node->next = NULL;
+  return new_node;
+}
+
+
