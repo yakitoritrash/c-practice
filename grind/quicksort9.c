360fa46b7345eff2b4940f17fe65d2b0230659b9
Arch me <kishudwivedi30@gmail.com>
2025-04-05 11:23:12 +0530

    dsfsfd

--- grind/quicksort9.c
+++ grind/quicksort9.c
@@ -4,7 +4,7 @@ int partiton(int arr[], int lo, int hi) {
   int pivot = arr[hi];
   int idx = lo - 1;
 
-  for (int i = 0; i <= hi; i++) {
+  for (int i = lo; i < hi; i++) {
     if (arr[i] < pivot) {
       idx++;
       int tmp = arr[i];
