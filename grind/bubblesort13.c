2e7ae0bb20b3b1a03000f363048c8818e000c2db
Arch me <kishudwivedi30@gmail.com>
2025-04-07 10:21:39 +0530

    2313asda

--- grind/bubblesort13.c
+++ grind/bubblesort13.c
@@ -3,7 +3,7 @@
 void bubblesort(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size - 1 - i; j++) {
-      if (arr[j] < arr[j + 1]) {
+      if (arr[j] > arr[j + 1]) {
         int tmp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = tmp;
