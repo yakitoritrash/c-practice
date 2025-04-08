f4f8c0b10bbfa26d5a9ea153a63229e11f2c3ceb
Arch me <kishudwivedi30@gmail.com>
2025-04-08 12:21:35 +0530

    3424

--- grind/quicksort12.c
+++ grind/quicksort12.c
@@ -26,5 +26,25 @@ void qs(int arr[], int lo, int hi) {
   int pivotIdx = partition(arr, lo, hi);
   qs(arr, lo, pivotIdx - 1);
   qs(arr, pivotIdx + 1, hi);
+}
 
+void quicksort(int arr[], int size) {
+  qs(arr, 0, size - 1);
+}
+
+int main() {
+  int arr[] = {2, 1, 45, 34 ,132, 65, 343, 2, 1, 90, 67, 53, 89};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  printf("\n");
+
+  quicksort(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
 }
