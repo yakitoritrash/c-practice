f4a86244ecb06ef175354d6be875fb84db546c3e
Arch me <kishudwivedi30@gmail.com>
2025-04-09 01:54:22 +0530

    3242

--- grind/linkedlists1.c
+++ grind/linkedlists1.c
@@ -9,10 +9,20 @@ typedef struct node_t {
 node_t *create_node(int value) {
   node_t *new_node = malloc(sizeof(node_t));
   if (!new_node) {
-    printf("Memory allocation failed.");
+    printf("Memory allocation failed.\n");
     return NULL;
   }
 
   new_node -> data = value;
   new_node -> next = NULL;
+
+  return new_node;
+}
+
+node_t *insert_at_head(node_t *head, int value) {
+  *new_node = create_node(value);
+  if (new_node == NULL) {
+    printf("ERROR\n");
+  }
+
 }
