dfcdab7c3ab74b2c9ba055addf6b4e3c236c57ff
Arch me <kishudwivedi30@gmail.com>
2025-04-10 12:09:22 +0530

    32423

---
+++ grind/linkelist3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int data;
+  struct node_t *next;
+} node_t;
+
+node_t *create_node(int value) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    printf("Memory allocation failed.\n");
+  }
+
+  new_node->data = value;
+  new_node->next = NULL;
+  return new_node;
+} 
+
+node_t *insert_at_head(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return head;
+  }
+  
+  new_node->next = head;
+  return head;
+}
+
+node_t *insert_at_end(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+
+  if (new_node == NULL) {
+    return head;
+  }
+  if (head == NULL) {
+    return new_node;
+  }
+
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    tmp = tmp -> next;
+  }
+  tmp -> next = new_node;
+  return head;
+}
+
+
