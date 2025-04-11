6318a8b2563b6a8ba941fb5a3b0641e02ec171f7
Arch me <kishudwivedi30@gmail.com>
2025-04-11 10:36:27 +0530

    324234

--- grind/linked5.c
+++ grind/linked5.c
@@ -61,6 +61,36 @@ node_t *insert_at_position(node_t *head, int value, int position) {
   return head;
 }
 
+node_t *delete_at_position(node_t *head, int position) {
+
+  if (head == NULL) {
+    return NULL;
+  }
+  if (position == 0) {
+    node_t *to_delete = head;
+    head = head->next;
+    free(to_delete);
+    return head;
+  }
+  node_t *tmp = head;
+  for (int i = 0; i < position - 1; i++) {
+    if (tmp == NULL || tmp->next == NULL) {
+      printf("Position out of bounds\n");
+      return head;
+    }
+    tmp = tmp->next;
+  }
+  node_t *to_delete = tmp->next;
+  if (to_delete == NULL) {
+    printf("Nothing to delete at that position.\n");
+    return head;
+  }
+
+  tmp->next = to_delete->next;
+  free(to_delete);
+  return head;
+}
+
 int main() {
 
   node_t *head = NULL;
@@ -70,10 +100,14 @@ int main() {
   head = insert_at_head(head, 6);
   head = insert_at_end(head, 9);
   head = insert_at_position(head, 20, 1);
+  head = delete_at_position(head, 2);
   node_t *tmp = head;
   while (tmp != NULL) {
     printf("%d -> ", tmp->data);
     tmp = tmp->next;
   }
-  printf("NULL");
+  printf("NULL\n");
+
+  printf("%d\n", sizeof(head));
+  printf("%d", sizeof(node_t));
 }
