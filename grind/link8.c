85eeb3d31880451b0bd94492526614f304e264e2
Arch me <kishudwivedi30@gmail.com>
2025-04-12 11:30:54 +0530

    @#423

--- grind/link8.c
+++ grind/link8.c
@@ -28,5 +28,15 @@ node_t *insert_at_head(node_t *head, int value) {
 
 node_t *insert_at_end(node_t *head, int value) {
   node_t *new_node = create_node(value);
-
+  if (new_node == NULL) {
+    return head;
+  }
+  if (head == NULL) {
+    return new_node;
+  }
+  node_t *tmp = head;
+  while (tmp->next != NULL) {
+    tmp = tmp->next;
+  }
+  
 }
