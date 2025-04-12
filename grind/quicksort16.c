110ca657efa3da9e2a2cb484db84b99438069a3f
Arch me <kishudwivedi30@gmail.com>
2025-04-12 10:51:02 +0530

    34234

--- grind/quicksort16.c
+++ grind/quicksort16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int partition(int arr[], int lo, int hi) {
-  int pivot = hi;
+  int pivot = arr[hi];
   int idx = lo - 1;
-  for (int i = lo; i <= hi; i++) {
+  for (int i = lo; i < hi; i++) {
     if (arr[i] < pivot) {
       idx++;
       int tmp = arr[i];
