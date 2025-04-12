6981c3eb9499a12987e76c5db80afe7c1a483647
Arch me <kishudwivedi30@gmail.com>
2025-04-12 12:27:28 +0530

    32423$

--- grind/link8.c
+++ grind/link8.c
@@ -59,12 +59,59 @@ node_t *insert_at_position(node_t *head, int value, int position) {
   return head;
 }
 
+node_t *delete_a_position(node_t *head, int position) {
+  if (position == 0) {
+    node_t *to_be_deleted = head;
+    head = head->next;
+    free(to_be_deleted);
+    return head;
+  }
+  node_t *tmp = head;
+  for (int i = 0; i < position - 1; i++) {
+    if (tmp == NULL || tmp->next == NULL) {
+      printf("Out of bounds");
+    }
+    tmp = tmp->next;
+  }
+  node_t *to_be_deleted = tmp->next;
+  tmp->next = to_be_deleted->next;
+  free(to_be_deleted);
+  return head;
+}
+
+node_t *delete_by_value(node_t *head, int value) {
+  if (head == NULL) {
+    return NULL;
+  }
+  if (value == head->data) {
+    node_t *to_be_deleted = head;
+    head = head->next;
+    free(to_be_deleted);
+    return head;
+  }
+  node_t *tmp = head;
+  while (tmp->next != NULL && tmp->next->data != value) {
+    tmp = tmp->next;
+  }
+  if (tmp->next == NULL) {
+    printf("Nothing to delete here.\n");
+    return head;
+  }
+
+  node_t *to_be_deleted = tmp->next;
+  tmp->next = to_be_deleted->next;
+  free(to_be_deleted);
+  return head;
+}
+
 int main() {
   node_t *head = NULL;
   head = insert_at_head(head, 5);
   head = insert_at_head(head, 10);
   head = insert_at_end(head, 15);
   head = insert_at_position(head, 2, 1);
+  head = delete_a_position(head, 2);
+  head = delete_by_value(head, 5);
 
   node_t *tmp = head;
   while(tmp != NULL) {
