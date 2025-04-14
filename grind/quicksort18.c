e2b21ff121fef0b51ef9878f50683895fb8df5b4
Arch me <kishudwivedi30@gmail.com>
2025-04-14 18:33:37 +0530

    32423

---
+++ grind/quicksort18.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int partition(int arr[], int lo, int hi) {
+  int pivot = arr[hi];
+  int idx = lo - 1;
+
+  for (int i = 0; i <= hi; i++) {
+    if (arr[i] < pivot) {
+      idx++;
+      int tmp = arr[i];
+      arr[i] = arr[idx];
+      arr[idx] = tmp;
+    }
+  }
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+  return idx;
+}
+
+int qs(int arr[], int lo, int hi) {
+  if (lo >= hi) {
+    return;
+  }
+
+  int pivotIdx = partition(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, lo, pivotIdx - 1);
+}
