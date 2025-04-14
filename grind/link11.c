edd7b922c44e2baf9ceb8e331463fd7f9cdcd6bb
Arch me <kishudwivedi30@gmail.com>
2025-04-14 23:11:15 +0530

    324243

--- grind/link11.c
+++ grind/link11.c
@@ -9,6 +9,31 @@ typedef struct node_t {
 node_t *create_node(int value) {
   node_t *new_node = malloc(sizeof(node_t));
   if (!new_node) {
-
+    printf("Memory allocation failed.");
   }
+
+  new_node->data = value;
+  new_node->next = NULL;
+  return new_node;
+}
+
+node_t *insert_at_head(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return head;
+  }
+  new_node->next= head;
+  return new_node;
+}
+
+node_t *insert_at_end(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return head;
+  }
+
+  if (head == NULL) {
+    return new_node;
+  }
+
 }
