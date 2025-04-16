f6ac761632ef0e33b4ee2194289bff89ac9c0de2
Arch me <kishudwivedi30@gmail.com>
2025-04-16 10:17:49 +0530

    234

---
+++ grind/quicksort20.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int partition(int arr[], int lo, int hi){
+  int pivot = arr[hi];
+  int idx = lo - 1;
+
+  for (int i = lo; i <= hi; i++) {
+    if (arr[i] < pivot) {
+      idx++;
+      int tmp = arr[j];
+      arr[j] = arr[j + 1];
+      arr[j + 1] = tmp;
+    } 
+  }
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+  return idx;
+}
+
+void qs(int arr[], int lo, int hi) {
+  if (lo >= hi) {
+    return;
+  }
+  int pivotIdx = partition(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1; hi);
+}
+
+void quicksort(int arr[], int size) {
+  qs(arr, 0, size - 1)
+}
+
+int main() {
+  int arr[] = {4, 5, 12, 543, 43, 5453, 676, 123, 89};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  printf("\n");
+
+  quicksort(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+}
