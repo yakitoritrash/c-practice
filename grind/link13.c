934960e0fe802b1581b4ef28386a10707326450f
Arch me <kishudwivedi30@gmail.com>
2025-04-16 10:55:40 +0530

    #@423$

---
+++ grind/link13.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int data;
+  struct node_t *next;
+  struct node_t *prev;
+} node_t;
+
+ node_t *create_node(int value) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    printf("Memory allocation failed.\n");
+    return NULL;
+  }
+
+  new_node->data = value;
+  new_node->next = NULL;
+  new_node->prev = NULL;
+  return new_node;
+}
+
+node_t *insert_at_head(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return head;
+  }
+  new_node->next = head;
+  if (head != NULL) {
+    head->prev = new_node;
+  }
+  head = new_node;
+  return new_node;
+}
+
+node_t *insert_at_end(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return head;
+  }
+
+  if (head == NULL) {
+    return new_node;
+  }
+
+}
+
