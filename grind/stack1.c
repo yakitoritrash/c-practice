5a7cf34dc3d2f381996cbf03e86698fea02bbc4c
Arch me <kishudwivedi30@gmail.com>
2025-04-19 12:27:10 +0530

    34234

--- grind/stack1.c
+++ grind/stack1.c
@@ -7,3 +7,21 @@ typedef struct {
   int items[MAX_SIZE];
   int top;
 } stack_t;
+
+stack_t *init_stack() {
+  stack_t *new_stack = malloc(sizeof(stack_t));
+  if (new_stack == NULL) {
+    return NULL;
+  }
+  new_stack->top = -1;
+  return new_stack;
+}
+
+int is_empty(stack_t *stack) {
+  if (stack->top == -1) {
+    return 1;
+  }
+  return 0;
+}
+
+int is_full(stack_t *stack
