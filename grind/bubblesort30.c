e61323fec03e153d48593835c77232d07074ef60
Arch me <kishudwivedi30@gmail.com>
2025-04-24 23:13:06 +0530

    33424

--- grind/bubblesort30.c
+++ grind/bubblesort30.c
@@ -1,3 +1,27 @@
 #include <stdio.h>
 
+void bubblesort(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+}
 
+int main() {
+  int arr[] = {98, 232, 65, 3, 65, 232, 78, 43, 12, 65, 23};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+  bubblesort(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+}
