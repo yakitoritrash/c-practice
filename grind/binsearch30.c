ca591bf0a4e4e054cb4ce0b863782cff7a219db9
Arch me <kishudwivedi30@gmail.com>
2025-04-24 22:55:31 +0530

    234234

--- grind/binsearch30.c
+++ grind/binsearch30.c
@@ -4,6 +4,22 @@ int binsearch(int arr[], int size, int n) {
   int hi = size - 1;
   int lo = 0;
   while (lo <= hi) {
-
+    int m = lo + (hi - lo) / 2;
+    int v = arr[m];
+    if (v == n) {
+      return m;
+    } else if (v > n) {
+      hi = m - 1;
+    } else {
+      lo = m + 1;
+    }
   }
+  return -1;
+}
+
+int main() {
+  int arr[] = {3, 4, 5, 6, 7, 8, 9};
+  int n = 3;
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int result = binsearch
 }
