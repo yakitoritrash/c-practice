4ba52ba62be4c7bef9bbc3c1f15b13d8c7da7e85
Arch me <kishudwivedi30@gmail.com>
2025-04-29 18:47:13 +0530

    @34324@

--- grind/dlink7.c
+++ grind/dlink7.c
@@ -7,4 +7,17 @@ typedef struct node_t {
   struct node_t *prev;
 } node_t;
 
-node_t *create_node(
+node_t *create_node(int value) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    printf("Memory allocation failed.\n");
+  }
+  new_node->data = value;
+  new_node->next = NULL;
+  new_node->prev = NULL;
+  return new_node;
+}
+
+node_t *prepend(node_t *head, int value) {i
+
+}
