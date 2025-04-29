fd724915ae2eb86fe98506649fa96bd25086a08d
Arch me <kishudwivedi30@gmail.com>
2025-04-29 20:56:42 +0530

    @34324@

--- grind/dlink7.c
+++ grind/dlink7.c
@@ -11,6 +11,7 @@ node_t *create_node(int value) {
   node_t *new_node = malloc(sizeof(node_t));
   if (!new_node) {
     printf("Memory allocation failed.\n");
+    return NULL;
   }
   new_node->data = value;
   new_node->next = NULL;
@@ -31,4 +32,13 @@ node_t *prepend(node_t *head, int value) {
   return new_node;
 }
 
+node_t *append(node_t *head, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return head;
+  }
+  if (head == NULL) {
+    return new_node;
+  }
 
+}
