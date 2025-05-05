a04de39bac23536433d2134ed08a4fd9f0517621
Arch me <kishudwivedi30@gmail.com>
2025-05-05 19:12:35 +0530

    234234

--- grind/quicksort34.c
+++ grind/quicksort34.c
@@ -6,10 +6,42 @@ int partition(int arr[], int lo, int hi) {
 
   for (int i = lo; i <= hi; i++) {
     if (arr[i] < pivot) {
+      idx++;
       int tmp = arr[i];
       arr[i] = arr[idx];
       arr[idx] = tmp;
     }
   }
-  
+  idx++;
+  arr[hi] = arr[idx];
+  arr[idx] = pivot;
+  return idx;
+}
+
+void qs(int arr[], int lo, int hi) {
+  if (lo >= hi) {
+    return;
+  }
+  int pivotIdx = partition(arr, lo, hi);
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+}
+
+void quicksort(int arr[], int size) {
+  qs(arr, 0, size - 1);
+}
+
+int main() {
+
+  int arr[] = {87, 3, 21, 54, 23, 78, 43, 12, 76, 54};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+  quicksort(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
 }
