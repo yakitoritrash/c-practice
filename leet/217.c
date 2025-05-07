d00e056708ab56378604f8336d21188a7454080a
Arch me <kishudwivedi30@gmail.com>
2025-05-07 18:34:47 +0530

    234234

--- leet/217.c
+++ leet/217.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool duplicates_in_arr(int arr[]) {
-    
+bool duplicates_in_arr(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      if (arr[i] == arr[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+int main() {
+  int arr[] = {3, 4, 5, 6, 8, 2, 3};
+  int size = sizeof arr / sizeof arr[0];
 }
