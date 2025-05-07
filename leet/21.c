f4e3e1c624ee7e7ee5eb48829d688d418ac1dc10
Arch me <kishudwivedi30@gmail.com>
2025-05-07 17:14:22 +0530

    24234

--- leet/21.c
+++ leet/21.c
@@ -13,6 +13,28 @@ ListNode_t *mergeTwoLists(ListNode_t *head1, ListNode_t *head2) {
   if (head2 == NULL) {
     return head1;
   }
-  
+  if (head1->val <= head2->val) {
+    head1->next = mergeTwoLists(head1->next, head2);
+    return head1;
+  } else {
+    head2->next = mergeTwoLists(head1, head2->next);
+    return head2;
+  }
 }
 
+void printlist(ListNode_t *tmp) {
+  while (tmp->next != NULL) {
+    printf("%d -> ", tmp->val);
+    tmp = tmp->next;
+  }
+  printf("NULL ");
+}
+
+ListNode_t *create_node(int val) {
+  ListNode_t *new_node = malloc(sizeof(ListNode_t));
+  if (!new_node) {
+    printf("Memory allocation failed.");
+    return NULL;
+  }
+
+}
