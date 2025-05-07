0dc52a5987fd9a751a3b5ff13bb4de0a54924543
Arch me <kishudwivedi30@gmail.com>
2025-05-07 19:11:39 +0530

    324234

--- leet/217.c
+++ leet/217.c
@@ -3,17 +3,15 @@
 
 bool duplicates_in_arr(int arr[], int size) {
   for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      if (arr[i] == arr[j]) {
-        return true;
-      }
-    }
+
   }
-  return false;
+  
 }
 
 int main() {
-  int arr[] = {3, 4, 5, 6, 8, 2, 3};
+  int arr[] = {4, 5, 6, 8, 2, 3};
   int size = sizeof arr / sizeof arr[0];
-  duplicates_in_arr(arr, size);
+  bool res = duplicates_in_arr(arr, size);
+  printf("%d\n", res);
+  return 0;
 }
