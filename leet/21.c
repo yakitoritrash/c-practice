3478d326bd8d1aaefc4978dc4ced1bff3f24644e
Arch me <kishudwivedi30@gmail.com>
2025-05-07 16:40:33 +0530

    24234

--- leet/21.c
+++ leet/21.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct ListNode {}
+typedef struct ListNode_t {
+  int val;
+  struct ListNode_t *next;
+} ListNode_t;
+
+ListNode_t *mergeTwoLists(ListNode_t *head1, ListNode_t *head2) {
+  if (head1 == NULL) {
+    return head2;
+  } 
+  if (head2 == NULL) {
+    return head1;
+  }
+  
+}
+
