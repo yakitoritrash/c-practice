01d039b735876a1366561df1db513a101ae9110d
Arch me <kishudwivedi30@gmail.com>
2025-05-09 03:49:22 +0530

    23423

--- grind/stack2.c
+++ grind/stack2.c
@@ -6,4 +6,50 @@ typedef struct node_t {
   struct node_t *prev;
 } node_t;
 
+node_t *create_node(int value) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    printf("Memory allocation failed.\n");
+    return NULL;
+  }
+  new_node->val = value;
+  new_node->prev = NULL;
+  return new_node;
+}
 
+node_t *push(node_t *top, int value) {
+  node_t *new_node = create_node(value);
+  if (new_node == NULL) {
+    return top;
+  }
+  if (top == NULL) {
+    return new_node;
+  }
+  new_node->prev = top;
+  top = new_node;
+  return new_node;
+}
+
+node_t *pop(node_t *top) {
+  if (top == NULL) {
+    printf("Nothing to delete.\n");
+    return NULL;
+  }
+  node_t *to_pop = top;
+  top = top->prev;
+  free(to_pop);
+  return top;
+}
+
+node_t *peek(node_t *top) {
+  if (top == NULL) {
+    printf("Stack is empty.\n");
+    return NULL;
+  }
+  printf("%d",top->val);
+  return top;
+}
+
+void print_stack(node_t *top) {
+  printf(""
+}
