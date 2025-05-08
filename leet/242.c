cc4085233a25c379ee34008dc44f0d749088d8ce
Arch me <kishudwivedi30@gmail.com>
2025-05-08 19:07:29 +0530

    @#4234

--- leet/242.c
+++ leet/242.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
+#define alphabet 26
 bool isAnagram(char *a, char *b) {
+  if (strlen(a) != strlen(b) {
+    return false;
+  }
+  
+  int hash1[alphabet] = {0};
+  int hash2[alphabet] = {0};
 
 }
