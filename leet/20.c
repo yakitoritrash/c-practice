fcfe77e04d48996f397a95fe88970354b8d8520b
Arch me <kishudwivedi30@gmail.com>
2025-05-14 15:39:53 +0530

    23424

---
+++ leet/20.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isValid(char *s) {
+  char stack[strlen(s)];
+  int top = -1;
+
+  for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+      stack[++top] = s[i];
+    } else {
+      if (top == -1 ||
+          (s[i] == ')' && stack[top] != '(') ||
+          (s[i] == '}' && stack[top] != '{') ||
+          (s[i] == ']' && stack[top] != '[')) {
+        return false;
+      }
+      top--;
+    }
+  }
+  return top == -1;
+}
+
+int main() {
+  char *s = "{}";
+  isValid(s);
+}
