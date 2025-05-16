394941f4755abca9aff425410c415ef54ca0461b
Arch me <kishudwivedi30@gmail.com>
2025-05-16 23:32:31 +0530

    34234

--- grind/bubblesort39.c
+++ grind/bubblesort39.c
@@ -15,5 +15,7 @@ void bubblesort(int arr[], int size) {
 int main() {
   int arr[] = {3, 4, 5, 6, 7, 8, 9};
   int size = sizeof(arr) / sizeof(arr[0]);
-
+  for (int i = 0; i < size; i++) {
+    printf
+  }
 }
