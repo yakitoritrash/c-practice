65df140c0cae06dc0ceb8d32e7f44cb6cf83b83e
Arch me <kishudwivedi30@gmail.com>
2025-05-21 16:07:38 +0530

    234234

--- leet/49.c
+++ leet/49.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define alphabets 26
 
-void compare(const void *a, const void *b) {
-
+char*** grpAnagrams(char** strs, int strsSize, int* returnSize, int** returnColumnSizes) {
+  for (int i = 0; i < strsSize; i++) {
+    int hash[alphabets] = {0};
+  }
 }
 int main() {
-
 }
