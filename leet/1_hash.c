293d295da39c244170de5caed7f7759eb704791c
Arch me <kishudwivedi30@gmail.com>
2025-05-21 12:16:04 +0530

    234234

--- leet/1_hash.c
+++ leet/1_hash.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #define HASH_SIZE 10000
@@ -17,6 +18,32 @@ hashmap_t *create_map() {
   hashmap_t *map = malloc(sizeof(hashmap_t));
   map->buckets = calloc(HASH_SIZE, sizeof(hashnode_t*));
   return map;
+}
+
+int hash(char *key) {
+  unsigned long hash = 0;
+  int c;
+  while ((c = *key++)) {
+    hash = hash * 31 + c;
+  }
+  return hash % HASH_SIZE;
+}
+
+void put(hashmap_t *map, char *key, int value) {
+  int bucketIdx = hash(key);
+  hashnode_t *new_node = map->buckets[bucketIdx];
+  if (new_node != NULL) {
+    if (strcmp(new_node->key, key) == 0) {
+      new_node->value = value;
+      return;
+    }
+    new_node = new_node->next;
+  }
+  hashnode_t *new_node1 = malloc(sizeof(hashnode_t));
+  new_node1->key = key;
+  new_node1->value = value;
+  new_node1->next = map->buckets[bucketIdx];
+  map->buckets[bucketIdx] = new_node1;
 }
 
 
