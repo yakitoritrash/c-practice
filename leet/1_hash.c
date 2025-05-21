1c17a5f0b0105ba5e04fd8e3750ea53bc7a61e2c
Arch me <kishudwivedi30@gmail.com>
2025-05-21 11:16:33 +0530

    2213

---
+++ leet/1_hash.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HASH_SIZE 10000;
+
+typedef struct hashnode_t {
+  char *key;
+  int value;
+  struct hashnode_t *next;
+} hashnode_t;
+
+typedef struct hashmap_t {
+  hashnode_t* buckets[HASH_SIZE];
+} hashmap_t;
