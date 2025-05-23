80a10bad0dfb19258eead929faf2c09a10e7d743
Arch me <kishudwivedi30@gmail.com>
2025-05-23 10:03:49 +0530

    mod2

--- grind/mod2.c
+++ grind/mod2.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main() {
-  int t;
-  scanf("%d", &t);
-  while (t--) {
-    int n;
-    scanf("%d", &n);
-
-    int a[n];
-    int o = 0, e = 0;
-
-    for (int i = 0; i < n; i++) {
-      scanf("d", &a[i]);
-      if (a[i] % 2 == 1) {
-        o++;
-      } else {
-        e++;
-      }
+    int t;
+    scanf("%d", &t);
+    
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        
+        int a[n];
+        int o = 0, e = 0;
+        
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+            if (a[i] % 2 == 1) {
+                o++;
+            } else {
+                e++;
+            }
+        }
+        
+        if ((e % 2 == 1) && (o % 2 == 1)) {
+            printf("0\n");
+        } else {
+            printf("1\n");
+        }
     }
-
-    if ((e % 2 == 1) && (o % 2 == 1)) {
-      printf("0\n");
-    } else {
-      printf("1\n");
-    }
-  }
-  return 0;
+    
+    return 0;
 }
