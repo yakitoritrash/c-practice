7fd7f262b8a590e18ab0f73a04cadf81afb2a229
Arch me <kishudwivedi30@gmail.com>
2025-05-26 12:35:17 +0530

    @#4234

---
+++ grind/bintree1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int val;
+  struct node_t *left;
+  struct node_t *right;
+} node_t;
+
+node_t *create_node(int value) {
+  node_t *new_node = malloc(sizeof(node_t));
+
+}
