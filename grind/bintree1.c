b4f17245739dfe7d6b0235fc783680584fac7b00
Arch me <kishudwivedi30@gmail.com>
2025-05-26 12:37:48 +0530

    234234

--- grind/bintree1.c
+++ grind/bintree1.c
@@ -9,5 +9,15 @@ typedef struct node_t {
 
 node_t *create_node(int value) {
   node_t *new_node = malloc(sizeof(node_t));
-
+  if (!new_node) {
+    printf("Memory allocation failed.\n");
+    return NULL;
+  }
+  new_node->val = value;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
 }
+
+:w
+
