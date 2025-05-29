f8c898f03fd1b7f1ce0041ba19c0cf1d149d5729
Arch me <kishudwivedi30@gmail.com>
2025-05-29 22:59:28 +0530

    23424'

--- grind/bubblesort40.c
+++ grind/bubblesort40.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 void bubblesort(int arr[], int size) {
-  for (int i = 0; 
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
 }
