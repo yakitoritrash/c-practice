0cfc5b5356943d1ee168ed0a144c9b1a9ca01ee2
Arch me <kishudwivedi30@gmail.com>
2025-05-30 10:45:04 +0530

    @#42#]

--- grind/dlink10.c
+++ grind/dlink10.c
@@ -19,4 +19,19 @@ node_t *create_node(int val) {
   return new_node;
 }
 
+node_t *prepend(node_t *head, int val) {
+  node_t *new_node = create_node(val); 
+  if (new_node == NULL) {
+    return head;
+  }
+  new_node->next = head;
+  if (new_node->next != NULL) {
+    new_node->next->prev = new_node->next;
+  }
+  head = new_node;
+  return new_node;
+}
 
+node_t *append(node_t *head, int val) {
+  node_t *new_node = create_node(val);
+}
