97a29cadb8f7660b6383062daa8f063be142169e
Arch me <kishudwivedi30@gmail.com>
2025-06-07 21:21:57 +0530

    2342:

---
+++ grind/binsearch46.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int binsearch(int arr[], int size, int n) {
+  int hi = size - 1;
+  int lo = 0;
+
+  while (hi >= lo) {
+    int m = lo + (hi - lo) / 2;
+    int v = arr[m];
+
+    if (v == n) {
+      return m;
+    } else if (v > n) {
+      hi = m - 1;
+    } else {
+      lo = m + 1;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  int arr[] = {4, 5, 6, 7 , 8, 9, 12};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int n = 9;
+  int result = binsearch(arr, size, n);
+
+  printf("%d", result);
+}
