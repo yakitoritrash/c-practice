487e176da8186619928a76c284e35504ef3181b1
Arch me <kishudwivedi30@gmail.com>
2025-06-08 21:03:31 +0530

    23424

--- grind/quicksort45.c
+++ grind/quicksort45.c
@@ -27,4 +27,23 @@ void qs(int arr[], int lo, int hi) {
   qs(arr, pivotIdx + 1, hi);
 }
 
+void quicksort(int arr[], int size) {
+  qs(arr, 0, size - 1);
+}
 
+int main() {
+  int arr[] = {65, 23, 12, 76, 45, 23, 12, 889, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+  
+  printf("\n");
+
+  quicksort(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+}
