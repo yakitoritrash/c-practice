f6833ef1d767dd2a687b0d5e5ff7f2644c3e67c9
Arch me <kishudwivedi30@gmail.com>
2025-06-16 14:32:49 +0530

    217

---
+++ leet/contains_duplicate.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int comp(const void *a, const void *b) {
+  return (*(int *)a - *(int *)b);
+}
+bool contains_duplicate (int arr[], int size) {
+  qsort(arr, size, sizeof(arr[0]), comp);
+  for (int i = 0; i < size; i++) {
+    if (arr[i] == arr[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+int main() {
+  int arr[] = {3, 4, 21, 12, 64, 12, 6, 2, 4};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int result = (int)contains_duplicate(arr, size);
+}
