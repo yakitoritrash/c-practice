e4c9c30537252a0e502782eb4bea8f1517efec39
Arch me <kishudwivedi30@gmail.com>
2025-07-02 12:16:32 +0530

    dsaf

--- grind/quicksort47.c
+++ grind/quicksort47.c
@@ -3,7 +3,7 @@
 int partition(int arr[], int lo, int hi) {
   int pivot = arr[hi];
   int idx = lo - 1;
-  for (int i = lo; i < hi; i++) {
+  for (int i = lo; i <= hi; i++) {
     if (arr[i] < pivot) {
       idx++;
       int tmp = arr[i];
@@ -37,6 +37,8 @@ int main() {
   for (int i = 0; i < size; i++) {
     printf("%d ", arr[i]);
   }
+
+  printf("\n");
 
   quicksort(arr, size);
 
