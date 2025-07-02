72a91e062252cd3a4dd55bb5a4d7c25d18f546a6
Arch me <kishudwivedi30@gmail.com>
2025-07-02 09:11:27 +0530

    fsadf

---
+++ grind/binsearch56.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int binsearch(int arr[], int size, int n) {
+  int hi = size - 1;
+  int lo = 0;
+  while (lo < hi) {
+    int m = lo + (hi - lo) / 2;
+    int v = arr[m];
+    if (v == n) {
+      return m;
+    } else if (v > n) {
+      hi = m;
+    } else {
+      lo = m - 1;
+    }
+  }
+  return -1;
+}
