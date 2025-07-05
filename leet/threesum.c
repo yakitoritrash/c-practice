c0affb2206609ed8712afb70f259fe1ad973976c
Arch me <kishudwivedi30@gmail.com>
2025-07-05 20:44:35 +0530

    three sum

--- leet/threesum.c
+++ leet/threesum.c
@@ -19,12 +19,15 @@ int** threesum(int arr[], int size, int* returnSize) {
     while (left < right) {
       int sum = arr[i] + arr[left] + arr[right];
       if (sum == 0) {
+        res[*returnSize] = malloc(3 * sizeof(int));
         res[*returnSize][0] = arr[i];
         res[*returnSize][1] = arr[left];
         res[*returnSize][2] = arr[right];
         (*returnSize)++;
         left++;
         right--;
+        while (left < right && arr[left] == arr[left - 1]) left++;
+        while (left < right && arr[right] == arr[right + 1]) right--;
       } else if (sum > 0) {
         right--;
       } else {
@@ -36,14 +39,17 @@ int** threesum(int arr[], int size, int* returnSize) {
 }
 
 int main() {
-  int arr[] = {-1, -1, 0, 2, -4};
+  int arr[] = {-1, -1, 0, 2, -1, -4};
   int size = sizeof(arr) / sizeof(arr[0]);
   int returnSize;
   for (int i = 0; i < size; i++) {
     printf("%d ", arr[i]);
   }
+  printf("\n");
   int** result = threesum(arr, size, &returnSize);
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i < returnSize; i++) {
     printf("%d %d %d", result[i][0], result[i][1], result[i][2]);
   }
+  free(result);
+  return 0;
 }
