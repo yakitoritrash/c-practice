11803a38e5ca0320971dbf4c8055bf949d30a80d
Arch me <kishudwivedi30@gmail.com>
2025-07-24 23:03:16 +0530

    bin tree

--- grind/bintree2.c
+++ grind/bintree2.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct leaf_t {
+typedef struct root_t {
   int val;
-  struct leaf_t *left;
-  struct leaf_t *right;
-} leaf_t;
+  struct root_t *left;
+  struct root_t *right;
+} root_t;
+
+root_t *create_node(int value) {
+  root_t *new_root = malloc(sizeof(root_t));
+  
+  return new_root;
+}
