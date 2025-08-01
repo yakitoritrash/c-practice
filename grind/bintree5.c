e8a8803e3a3b1201f8827abd65e5caab1f86a1de
Arch me <kishudwivedi30@gmail.com>
2025-08-01 16:03:48 +0530

    bin tree

--- grind/bintree5.c
+++ grind/bintree5.c
@@ -9,5 +9,17 @@ typedef struct node_t {
 
 node_t *create_node(int val) {
   node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val= val;
+  new_node->left = NULL;
+  new_node->right = NULL;
   return new_node;
+}
+
+node_t *insert_node(int val, node_t *root) {
+  node_t *new_node = create_node(val);
+  
+  return root;
 }
