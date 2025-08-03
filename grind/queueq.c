845eb671fd915c08a4b9730e7816f3358597f127
Arch me <kishudwivedi30@gmail.com>
2025-08-04 00:01:05 +0530

    queue ques

--- grind/queueq.c
+++ grind/queueq.c
@@ -1,4 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node_t {
+  int val;
+  struct node_t *next;
+} node_t;
 
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->next = NULL;
+  return new_node;
+}
+
+node_t *enqueue(int val, node_t *head) {
+  node_t *new_node = create_node(val);
+  if (head == NULL) {
+    return new_node;
+  }
+  new_node->next = head;
+  head = new_node;
+  return head;
+}
+
+node_t *reversek(node_t *head, int k) {
+  node_t *tmp = head;
+  node_t *next = NULL;
+  node_t *prev = NULL;
+  int count = 0;
+  while (tmp != NULL && count < k) {
+    next = tmp->next;
+    tmp->next = prev;
+    prev = tmp;
+    tmp = next;
+    count++;
+  }
+  if (head != NULL) {
+    head->next = tmp;
+  }
+  return prev;
+}
+
+void printlist(node_t *head) {
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    printf("%d ", tmp->val);
+    tmp = tmp->next;
+  }
+}
+int main() {
+  node_t *head = NULL;
+  head = enqueue(5, head);
+  head = enqueue(6, head);
+  head = enqueue(7, head);
+  head = enqueue(9, head);
+  head = enqueue(10, head);
+  head = enqueue(11, head);
+  printlist(head);
+  printf("\n");
+  printlist(reversek(head, 3));
+}
