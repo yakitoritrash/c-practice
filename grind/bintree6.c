2bece1296b696b896eb6f0edf4a671f2ec9714f8
Arch me <kishudwivedi30@gmail.com>
2025-08-06 21:00:27 +0530

    bin tree

--- grind/bintree6.c
+++ grind/bintree6.c
@@ -42,4 +42,12 @@ void inordertraverse(node_t *root) {
 
 int main() {
   int n;
+  scanf("%d", &n);
+  node_t *root  = NULL;
+  for (int i = 0; i < n; i++) {
+    int x;
+    scanf("%d", &x);
+    root = insert_node(x, root);
+  }
+  inordertraverse(root);
 }
