fd136330b6ff77aa01e34a44ff854a840216286d
Arch me <kishudwivedi30@gmail.com>
2025-08-07 14:51:35 +0530

    linkedlist

--- grind/linkedlists3.c
+++ grind/linkedlists3.c
@@ -22,13 +22,17 @@ node_t *prepend(int val, node_t *head) {
     return new_node;
   }
   new_node->next = head;
+  head = new_node;
   return head;
 }
 
 node_t *append(int val, node_t *head) {
   node_t *new_node = create_node(val);
+  if (head == NULL) {
+    return new_node;
+  }
   node_t *tmp = head;
-  while (tmp != NULL) {
+  while (tmp->next != NULL) {
     tmp = tmp->next;
   }
   tmp->next = new_node;
