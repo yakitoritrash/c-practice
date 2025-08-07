334e55ab28c428e95b04812a0f80f6e689a7f3a4
Arch me <kishudwivedi30@gmail.com>
2025-08-07 16:04:55 +0530

    invert tree

---
+++ grind/inverttree.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int val;
+  struct node_t *left;
+  struct node_t *right;
+} node_t;
+
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
+}
+
+node_t *insert_node(int val, node_t *root) {
+  node_t *new_node = create_node(val);
+  if (root == NULL) {
+    return new_node;
+  }
+  if (val < root->val) {
+    root->left = insert_node(val, root->left);
+  } else if (val >= root->val) {
+    root->right = insert_node(val, root->right);
+  }
+  return root;
+}
+
+
