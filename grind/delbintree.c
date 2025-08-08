b890b3eb2818838f516a901e6a335d2829a9b661
Arch me <kishudwivedi30@gmail.com>
2025-08-08 22:43:28 +0530

    bin tree delete

---
+++ grind/delbintree.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int val;
+  struct node_t *left;
+  struct node_t *right;
+} node_t;
+
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
+}
+
+node_t *insert_node(int val, node_t *root) {
+  if (root == NULL) {
+    return create_node(val);
+  }
+  if (val < root->val) {
+    root->left = insert_node(val ,root->left);
+  } else if (val >= root->val) {
+    root->right = insert_node(val, root->right);
+  }
+  return root;
+}
+
+void inordertraverse(node_t *root) {
+  inordertraverse(root->left);
+  printf("%d ", root->val);
+  inordertraverse(root->right);
+}
+
+
