0dad60cdcb748822a2458ef005b07d18649b908c
Arch me <kishudwivedi30@gmail.com>
2025-08-08 22:34:30 +0530

    delete from an array

--- grind/delarr.c
+++ grind/delarr.c
@@ -11,5 +11,21 @@ void del(int arr[], int *n, int key) {
 }
 
 int main() {
-
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int key;
+  scanf("%d", &key);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+  del(arr, &size, key);
+  for (int i = 0; i < n - 1; i++) {
+    printf("%d ", arr[i]);
+  }
 }
