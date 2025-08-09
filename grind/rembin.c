991b60382d5f436c517118be5a813ea1f915eda6
Arch me <kishudwivedi30@gmail.com>
2025-08-09 16:01:14 +0530

    bin tree find

---
+++ grind/rembin.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int val;
+  struct node_t *left;
+  struct node_t *right;
+} node_t;
+
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
+}
+
+node_t *insert_node(int val, node_t *root) {
+  if (root == NULL) {
+    return create_node(val);
+  }
+  if (val < root->val) {
+    root->left = insert_node(val, root->left);
+  } else if (val > root->val) {
+    root->right = insert_node(val, root->right);
+  }
+  return root;
+}
+
+bool find(node_t *root, int needle) {
+  if (root == NULL) {
+    return false;
+  }
+  if (root->val == needle) {
+    return false;
+  }
+  if (needle > root->val) {
+    return find(root->right, needle);
+  }
+  return find(root->left, needle);
+}
+
+
