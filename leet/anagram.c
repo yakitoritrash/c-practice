982fded2b0fa721ea6bc91c723aab353c1550472
Arch me <kishudwivedi30@gmail.com>
2025-08-09 11:14:43 +0530

    ana

---
+++ leet/anagram.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define alphabet 26
+
+bool anagram(char *t, char *s) {
+  if (strlen(t) != strlen(s)) {
+    return false;
+  }
+  int hash1[alphabet] = {0};
+  int hash2[alphabet] = {0};
+  for (int i = 0; s[i] != '\0'; i++) {
+    hash1[s[i] - 'a']++;
+    hash1[t[i] - 'a']++;
+  }
+  for (int i = 0; i < alphabet; i++) {
+    if (hash1[i] != hash2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+int main() {
+
+}
