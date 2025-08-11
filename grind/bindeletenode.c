5a13b017fb2478b33801e0b0f78900c3515cdef9
Arch me <kishudwivedi30@gmail.com>
2025-08-11 10:36:55 +0530

    bin tree

--- grind/bindeletenode.c
+++ grind/bindeletenode.c
@@ -44,4 +44,64 @@ bool find(int val, node_t *root) {
   return find(val, root->right);
 }
 
-:
+node_t *findmin(node_t *root) {
+  while (root && root->left != NULL) {
+    root = root->left;
+  }
+  return root;
+}
+
+node_t *delete_node(int needle, node_t *root) {
+  if (root == NULL) {
+    return root;
+  }
+  if (needle < root->val) {
+    root->left = delete_node(needle, root->left);
+  } else if (needle > root->val) {
+    root->right = delete_node(needle, root->right);
+  } else {
+    if (root->left == NULL) {
+      node_t *tmp = root->right;
+      free(root);
+      return tmp;
+    } else if (root->right == NULL) {
+      node_t *tmp = root->left;
+      free(root);
+      return tmp;
+    } else if (root->left == NULL && root->right == NULL) {
+      free(root);
+      return NULL;
+    }
+    node_t *min = findmin(root->right);
+    root->val = min->val;
+    root->right = delete_node(min->val, root->right);
+  }
+  return root;
+}
+
+void inorder(node_t *root) {
+  inorder(root->left);
+  printf("%d ", root->val);
+  inorder(root->right);
+}
+int main() {
+  int n;
+  scanf("%d", &n);
+  node_t *root = NULL;
+  for (int i = 0; i < n; i++) {
+    int x;
+    scanf("%d", &x);
+    root = insert_node(x, root);
+  }
+  int x;
+  scanf("%d", &x);
+  int y;
+  scanf("%d", &y);
+  inorder(root);
+  delete_node(x, root);
+  printf("\n");
+  inorder(root);
+  printf("\n");
+  int res = find(y, root);
+  printf("%d", res);
+}
