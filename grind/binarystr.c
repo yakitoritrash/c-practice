d91cbbcede9e93c5604d64f03886d8e599ae33e1
Arch me <kishudwivedi30@gmail.com>
2025-08-13 11:20:31 +0530

    binary string operations

--- grind/binarystr.c
+++ grind/binarystr.c
@@ -14,5 +14,24 @@ int binarystr(char *str) {
     int val = *str - '0';
     if (val != 0 && val != 1)
       return -1;
+    switch(op) {
+      case 'A': result = result & val; break;
+      case 'B': result = result | val; break;
+      case 'C': result = result ^ val; break;
+      default: return -1;
+    }
+    str++;
   }
+  return result;
+}
+
+int main() {
+  char expr[] = "1A0C1B0";
+  int res = binarystr(expr);
+  if (res != -1) {
+    printf("result: %d\n", res);
+  } else {
+    printf("Invalid expression.\n");
+  }
+  return 0;
 }
