26afdfe629b35b24aa1388e780db7d55801283c4
Arch me <kishudwivedi30@gmail.com>
2025-08-14 16:13:57 +0530

    queue question

--- grind/queueq1.c
+++ grind/queueq1.c
@@ -7,5 +7,20 @@ typedef struct node_t {
 } node_t;
 
 node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->next = NULL;
+  return new_node;
+}
 
+node_t *insert_node(int val, node_t *head) {
+  node_t *new_node = create_node(val);
+  if (head == NULL) {
+    return new_node;
+  }
+
+  return head;
 }
