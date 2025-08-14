1e77f3dfa5401d964bb36be04b9ed02771e0828c
Arch me <kishudwivedi30@gmail.com>
2025-08-14 16:24:19 +0530

    queue question

--- grind/queueq1.c
+++ grind/queueq1.c
@@ -21,6 +21,26 @@ node_t *insert_node(int val, node_t *head) {
   if (head == NULL) {
     return new_node;
   }
-
+  new_node->next = head;
+  head = new_node;
   return head;
+}
+
+node_t *dequeue(node_t *head) {
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    tmp = tmp->next;
+  }
+  node_t *to_delete = tmp;
+  printf("Node deleting is: %d", tmp->val);
+  free(to_delete);
+  return head;
+}
+
+void printlist(node_t *head) {
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    printf("%d ", tmp->val);
+    tmp = tmp->next;
+  }
 }
