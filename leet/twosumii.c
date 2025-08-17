9ecf2ed88ed3d0c8447e2c641a02e359dde62e5f
Arch me <kishudwivedi30@gmail.com>
2025-08-17 19:57:01 +0530

    hashmap

--- leet/twosumii.c
+++ leet/twosumii.c
@@ -14,6 +14,30 @@ int hash_func(int x) {
 
 int* two_sum_ii(int arr[], int k, int size) {
   entry_t* table = calloc(TABLE_SIZE, sizeof(entry_t));
+  int* res = malloc(sizeof(int) * 2);
+  for (int i = 0; i < size; i++) {
+    int complement = k - arr[i];
+    int hash = hash_func(complement);
+    while (table[hash].used) {
+      if (table[hash].key == complement) {
+        res[0] = table[hash].val;
+        res[1] = i;
+        free(table);
+        return res;
+      }
+      hash = (hash + 1) % TABLE_SIZE;
+    }
+    hash = hash_func(arr[i]);
+    while (table[hash].used) {
+      hash = (hash + 1) % TABLE_SIZE;
+    }
+    table[hash].used = 1;
+    table[hash].key = arr[i];
+    table[hash].val = i;
+  }
+  free(table);
+  free(res);
+  return NULL;
 }
 
 int main() {
