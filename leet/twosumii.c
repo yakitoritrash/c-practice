48f60d54207ec2bf58506374d56d0a08652e07fa
Arch me <kishudwivedi30@gmail.com>
2025-08-17 14:04:48 +0530

    hash table

--- leet/twosumii.c
+++ leet/twosumii.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define HASH_TABLE 100007
 int* two_sum_ii(int arr[], int k, int size) {
-  int* res = malloc(2 * sizeof(int));
 
-  int left = 0;
-  int right = size - 1;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == k) {
-      res[0] = left + 1;
-      res[1] = right + 1;
-      return res;
-    } else if (sum > k) {
-      right--;
-    } else {
-      left++;
-    }
-  }
-  free(res);
-  return NULL;
 }
 
 int main() {
