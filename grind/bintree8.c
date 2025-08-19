952f6ca0d5d7a4b092b891b083144731400562c2
Arch me <kishudwivedi30@gmail.com>
2025-08-19 10:19:37 +0530

    bin tree

---
+++ grind/bintree8.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t{
+  int val;
+  struct node_t *left;
+  struct node_t *right;
+} node_t;
+
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
+}
+
+node_t *insert_node(int val, node_t *root) {
+  if (root == NULL) {
+    return create_node(val);
+  }
+  if (root->val < val) {
+    root->right = insert_node(val, root->right);
+  } else if (root->val > val) {
+    root->left = insert_node(val, root->left);
+  }
+  return root;
+}
+
+node_t *findmin(node_t *root) {
+  while (root && root->left != NULL) {
+    root = root->left;
+  }
+  return root;
+}
+
+node_t *delete_node(node_t *root, int x) {
+  if (root == NULL) {
+    return NULL;
+  }
+  if (root->val < x) {
+    root->right = delete_node(root->right, x);
+  } else if (root->val > x) {
+    root->left = delete_node(root->left, x);
+  } else {
+    if (root->left == NULL) {
+      node_t *tmp = root->right;
+      free(root);
+      return tmp;
+    } else if (root->right == NULL) {
+      node_t *tmp = root->left;
+      free(root);
+      return tmp;
+    } else if (root->left == NULL && root->right == NULL) {
+      free(root);
+      return NULL;
+    }
+    node_t *min = findmin(root->right);
+    root->val = min->val;
+    root->right = delete_node(root->right, min->val);
+  }
+  return root;
+}
