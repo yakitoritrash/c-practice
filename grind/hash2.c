d994a42909dcc122a5b7dfbe0128e0b4eb915efd
Arch me <kishudwivedi30@gmail.com>
2025-08-19 10:03:16 +0530

    bin tree

--- grind/hash2.c
+++ grind/hash2.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct hashmapitem {
+  char *key;
+  int val;
+} hashmapitem_t;
+
+typedef struct hashmap {
+  int capacity;
+   
+
+} hashmap_t;
