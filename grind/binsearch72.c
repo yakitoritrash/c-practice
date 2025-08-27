d635237ffdcd5e96487dbd36825752da0d15f78b
Arch me <kishudwivedi30@gmail.com>
2025-08-27 10:12:48 +0530

    bin search

--- grind/binsearch72.c
+++ grind/binsearch72.c
@@ -17,4 +17,13 @@ int binsearch(int arr[], int size, int n) {
   return -1;
 }
 
-
+int main() {
+  int arr[] = {4, 5, 6, 7, 8, 9};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  int n = 5;
+  int res = binsearch(arr, size, n);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("%d", res);
+}
