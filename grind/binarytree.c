e59ea471534ee1abdd9b4befb1d91e457d426151
Arch me <kishudwivedi30@gmail.com>
2025-09-11 18:27:20 +0530

    binary tree

--- grind/binarytree.c
+++ grind/binarytree.c
@@ -8,5 +8,26 @@ typedef struct node_t {
 } node_t;
 
 node_t *create_node(int val) {
-
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
 }
+
+node_t *insert_node(int val, node_t *root) {
+  if (root == NULL) {
+    return create_node(val);
+  }
+  if (val <= root->val) {
+    root->left = insert_node(val, root->left);
+  } else if (val > root->val) {
+    root->right = insert_node(val, root->right);
+  }
+  return root;
+}
+
+node_t
