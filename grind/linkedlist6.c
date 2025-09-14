288bbe6df8648a3519420dd6f1d52ec25855183a
Arch me <kishudwivedi30@gmail.com>
2025-09-14 20:07:24 +0530

    append

--- grind/linkedlist6.c
+++ grind/linkedlist6.c
@@ -7,4 +7,43 @@ typedef struct node_t {
   struct node_t *prev;
 } node_t;
 
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+  }
+  new_node->val = val;
+  new_node->next = NULL;
+  new_node->prev = NULL;
+  return new_node;
+}
+
+node_t *append(int val, node_t *head) {
+  node_t *new_node = create_node(val);
+  if (head == NULL) {
+    return new_node;
+  }
+  node_t *tmp = head;
+  while (tmp->next != NULL) {
+    tmp = tmp->next;
+  }
+  tmp->next = new_node;
+  new_node->prev = tmp;
+  return head;
+}
+
+void printlist(node_t *head) {
+  node_t *tmp = head;
+  while (tmp != NULL) {
+    printf("%d <-> ", tmp->val);
+    tmp = tmp->next;
+  }
+  printf("NULL");
+}
+int main() {
+  node_t *head = NULL;
+  head = append(5, head);
+  head = append(1, head);
+  printlist(head);
+}
 
