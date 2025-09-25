e798cd8bc158f753cbc9a5a6895e59bcc4a3ad82
Arch me <kishudwivedi30@gmail.com>
2025-09-25 09:32:59 +0530

    binsearch

--- grind/binsearch74.c
+++ grind/binsearch74.c
@@ -6,7 +6,18 @@ int binsearch(int arr[], int size, int needle) {
   while (lo < hi) {
     int m = lo + (hi - lo) / 2;
     int v = arr[m];
-    int 
+    if (v == needle) {
+      return m;
+    } else if (v > needle) {
+      hi = m;
+    } else {
+      lo = m + 1;
+    }
   }
+  return -1;
+}
+
+int main() {
+
 }
 
