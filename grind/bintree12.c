94848bb56427dbdf05a1f2c9df21427628b0454a
Arch me <kishudwivedi30@gmail.com>
2025-10-06 15:09:00 +0530

    bin tree

--- grind/bintree12.c
+++ grind/bintree12.c
@@ -20,9 +20,23 @@ node_t* create_node(int val) {
 }
 
 node_t *append(node_t *root, int val) {
+  if (!root) {
+    return create_node(val);
+  }
+  if (root->val < val) {
+    root = append(root->right, val);
+  } else if (root->val > val) {
+    root = append(root->left, val);
+  }
+  return root;
+}
+
+void inorder(node_t *root) {
 
 }
 
+
 int main() {
+  node_t *root = NULL;
 
 }
