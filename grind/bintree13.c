df0b5e9ae9c18326f989ad2c7040a1455676bbdb
Arch me <kishudwivedi30@gmail.com>
2025-10-17 07:28:29 +0530

    bin tree

--- grind/bintree13.c
+++ grind/bintree13.c
@@ -40,7 +40,48 @@ void inoder(node_t *root) {
   inoder(root->right);
 }
 
+node_t *findmin(node_t *root) {
+  while (root && root->left != NULL) {
+    root = root->left;
+  }
+  return root;
+}
 
+node_t *deletenode(int x, node_t *root) {
+  if (root == NULL) {
+    return root;
+  }
+  if (x < root->val) {
+    root->left = deletenode(x, root->left);
+  } else if (x > root->val) {
+    root->right = deletenode(x, root->right);
+  } else {
+    if (root->left == NULL) {
+      node_t *tmp = root->right;
+      free(root);
+      return tmp;
+    } else if (root->right == NULL) {
+      node_t *tmp = root->left;
+      free(root);
+      return tmp;
+    } else if (root->right == NULL && root->left == NULL) {
+      free(root);
+      return NULL;
+    }
+    node_t *min = findmin(root->right);
+    root->val = min->val;
+    root->right = deletenode(min->val, root->right);
+  }
+  return root;
+}
+
+node_t *inverse(node_t *root) {
+  if (root == NULL) {
+    return NULL;
+  }
+  
+  return root;
+}
 
 int main() {
   node_t *root = NULL;
@@ -51,5 +92,10 @@ int main() {
     scanf("%d", &x);
     root = insert_node(x, root);
   }
+  inoder(root);
+  printf("\n");
+  int x;
+  scanf("%d", &x);
+  root = deletenode(x, root);
   inoder(root);
 }
