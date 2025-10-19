17f9e68adda6103e23fe13d65f76af17567958f9
Arch me <kishudwivedi30@gmail.com>
2025-10-19 16:11:58 +0530

    bin tree

---
+++ grind/bintree14.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_t {
+  int val;
+  struct node_t *left;
+  struct node_t *right;
+} node_t;
+
+node_t *create_node(int val) {
+  node_t *new_node = malloc(sizeof(node_t));
+  if (!new_node) {
+    return NULL;
+    printf("Memory allocation failed.\n");
+  }
+  new_node->val = val;
+  new_node->left = NULL;
+  new_node->right = NULL;
+  return new_node;
+}
+
+node_t *insert_node(int val, node_t *root) {
+  if (root == NULL) {
+    return create_node(val);
+  }
+  if (root->val < val) {
+    root->right = insert_node(val, root->right);
+  } else {
+    root->left = insert_node(val, root->left);
+  }
+  return root;
+}
+
+void inorder(node_t *root) {
+  if (root == NULL) {
+    return;
+  }
+  inorder(root->left);
+  printf("%d ", root->val);
+  inorder(root->right);
+}
+
+int main() {
+  node_t *root = NULL;
+  int n;
+  scanf("
+}
