f23c3f8481f7537376881ceff3103ead0333def5
Arch me <kishudwivedi30@gmail.com>
2025-11-28 12:42:43 +0530

    linked lists

--- grind/linkedlist6.c
+++ grind/linkedlist6.c
@@ -8,7 +8,7 @@ typedef struct node_t {
 } node_t;
 
 node_t *create_node(int val) {
-  node_t *new_node = malloc(sizeof(node_t));
+  noe_t *new_node = malloc(sizeof(node_t));
   if (!new_node) {
     return NULL;
   }
