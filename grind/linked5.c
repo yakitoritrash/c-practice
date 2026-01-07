9f0f07dad465f737047d6d1081a71cb3796d79b0
Arch me <kishudwivedi30@gmail.com>
2026-01-07 22:07:13 +0530

    linked list

--- grind/linked5.c
+++ grind/linked5.c
@@ -7,7 +7,7 @@ typedef struct node_t {
 } node_t;
 
 node_t *create_node(int value) {
-  node_t *new_node = malloc(sizeof(node_t));
+ node_t *new_node = malloc(sizeof(node_t));
   if (!new_node) {
     printf("Memory allocation failed.\n");
     return NULL;
